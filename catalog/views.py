f31868eb703bd82aa12b01dfae50003ca2d19cd6
soblin <hilo.soblin@gmail.com>
2021-01-01 08:46:15 +0900

    finished part8

--- catalog/views.py
+++ catalog/views.py
@@ -1,7 +1,8 @@
 # -*- coding: utf-8 -*-
 from django.shortcuts import render
 from django.views import generic
-from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+
 from catalog.models import Book, Author, BookInstance, Genre
 
 # Create your views here.
@@ -40,8 +41,9 @@ class LoanedBooksByUserListView(LoginRequiredMixin, generic.ListView):
     def get_queryset(self):
         return BookInstance.objects.filter(borrower=self.request.user).filter(status__exact='o').order_by('due_back')
 
-class BorrowedBooksAllListView(LoginRequiredMixin, generic.ListView):
+class BorrowedBooksAllListView(PermissionRequiredMixin, generic.ListView):
     model = BookInstance
+    permission_required = 'catalog.can_mark_returned'
     template_name = 'catalog/bookinstance_list_borrowed_all.html'
     paginate_by = 10
 
