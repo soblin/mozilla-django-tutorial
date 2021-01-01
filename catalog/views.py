9ffd9b850f252eafab7d9e526b1ce9f147081445
soblin <hilo.soblin@gmail.com>
2021-01-02 00:38:20 +0900

    finished

--- catalog/views.py
+++ catalog/views.py
@@ -5,6 +5,8 @@ from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMix
 from django.http import HttpResponseRedirect
 from django.urls import reverse
 from django.contrib.auth.decorators import login_required, permission_required
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 import datetime
 
@@ -79,3 +81,16 @@ def renew_book_librarian(request, pk):
         }
 
         return render(request, 'catalog/book_renew_librarian.html', context)
+
+class AuthorCreate(CreateVieww):
+    model = Author
+    fields = ['first_name', 'last_name', 'date_of_birth', 'date_of_death']
+    initial = {'date_of_birth': '11/06/2020'}
+
+class AuthorUpdate(UpdateView):
+    model = Author
+    fields = '__all__'
+
+class AuthorDelete(DeleteView):
+    model = Author
+    success_url = reverse_lazy('authors')
