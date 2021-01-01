2025dc1cd3012f72c29b90f6fa694d8e5bb4e7ea
soblin <hilo.soblin@gmail.com>
2021-01-02 01:54:53 +0900

    hid secret key

--- locallibrary/settings.py
+++ locallibrary/settings.py
@@ -21,7 +21,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'p((lh@q)uq!*yojp)obil$2*_v$#mq-1z^$69#dnvs=kuapr9m'
+SECRET_KEY = ''
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
